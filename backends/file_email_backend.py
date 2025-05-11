63dfaa034d999edd20958f85398b0ae0f52b5481
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-05-11 13:24:38 +0300

    create FileEmailBackend

---
+++ backends/file_email_backend.py
@@ -0,0 +1,35 @@
+import random
+from datetime import datetime
+from pathlib import Path
+
+from backends.base_email_backend import BaseEmailBackend
+
+
+class FileEmailBackend(BaseEmailBackend):
+    def __init__(self, directory: Path) -> None:
+        self.directory = directory
+        self.directory.mkdir(parents=True, exist_ok=True)
+
+    def get_filename(
+        self,
+        recipient: str,
+        subject: str,
+    ) -> str:
+        ts = datetime.now().strftime("%Y%m%d_%H%M%S")
+        rnd = random.randint(1_000_000, 9_999_999)
+        rec = recipient.replace("@", "_at_")
+        subj = subject.replace(" ", "_")
+        return f"{ts}.{rnd}._{rec}_{subj}.txt"
+
+    def send_email(
+        self,
+        recipient: str,
+        subject: str,
+        body: str,
+    ) -> None:
+        filename = self.get_filename(recipient, subject)
+        filepath = self.directory / filename
+        with filepath.open("w") as f:
+            f.write(f"To: {recipient}\n")
+            f.write(f"Subject: {subject}\n\n")
+            f.write(body)
