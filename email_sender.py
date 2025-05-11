66a7c59bcc02a8b54144efe03cdf73ee1c4146f4
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-05-11 13:24:50 +0300

    create EmailSender

---
+++ email_sender.py
@@ -0,0 +1,36 @@
+import logging
+
+from backends.base_email_backend import BaseEmailBackend
+
+log = logging.getLogger(__name__)
+
+
+class EmailSender:
+    def __init__(self, backend: BaseEmailBackend) -> None:
+        self.backend = backend
+
+    def send(
+        self,
+        recipient: str,
+        subject: str,
+        body: str,
+    ) -> None:
+        self.backend.send_email(
+            recipient,
+            subject,
+            body,
+        )
+
+    def send_with_template(
+        self,
+        recipient: str,
+        subject: str,
+        template: str,
+        **params,
+    ) -> None:
+        body = template.format(**params)
+        return self.send(
+            recipient=recipient,
+            subject=subject,
+            body=body,
+        )
