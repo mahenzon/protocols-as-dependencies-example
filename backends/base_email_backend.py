c608e545fc3b9105f5e5642082eeb102d4e2997c
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-05-11 13:24:29 +0300

    create BaseEmailBackend

---
+++ backends/base_email_backend.py
@@ -0,0 +1,10 @@
+from typing import Protocol
+
+
+class BaseEmailBackend(Protocol):
+    def send_email(
+        self,
+        recipient: str,
+        subject: str,
+        body: str,
+    ) -> None: ...
